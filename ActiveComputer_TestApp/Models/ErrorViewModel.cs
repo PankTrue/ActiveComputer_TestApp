4c08ab07b47f40b7d9fe8c09bb8612cf41021d1c
PankTrue <linkinpark02111@mail.ru>
2021-01-12 13:15:31 +0300

    default asp.net core app

---
+++ ActiveComputer_TestApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ActiveComputer_TestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
